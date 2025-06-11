406ab28b81f366744f52280a0fbececcf3a4e217
Mahesh Banduni <maheshbanduni9997@gmail.com>
2025-06-11 11:25:12 +0530

    Add files via upload

---
+++ client/src/app/dashboard/candidates/page.jsx
@@ -0,0 +1,8 @@
+export default function Candidates() {
+  return (
+    <div className=" max-w-screen-xl mx-auto">
+      <h1 className="text-2xl font-bold">Candidates</h1>
+      <p>Candidate list goes here.</p>
+    </div>
+  );
+}
