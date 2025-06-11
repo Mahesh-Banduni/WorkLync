2bf302a886f9f4b05e798d8f28f1d47d8d7c3989
Mahesh Banduni <maheshbanduni9997@gmail.com>
2025-06-11 17:06:03 +0530

    Delete client/src directory

--- client/src/app/dashboard/employees/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Employees() {
-  return (
-    <div className=" max-w-screen-xl mx-auto">
-      <h1 className="text-2xl font-bold">Candidates</h1>
-      <p>Candidate list goes here.</p>
-    </div>
-  );
-}
