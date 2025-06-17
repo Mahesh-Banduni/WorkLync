198ee8940173e3749d23370555ee089227980eed
Mahesh Banduni <maheshbanduni9997@gmail.com>
2025-06-17 15:00:19 +0530

    Delete server/src directory

--- server/src/middlewares/multer.js
+++
@@ -1,8 +0,0 @@
-const multer = require('multer');
-
-// In-memory storage — files won't be saved on disk
-const storage = multer.memoryStorage();
-
-const uploadDocument = multer({ storage });
-
-module.exports = { uploadDocument };
--- server/src/routes/auth.route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/auth.controller.js");
-
-router.post("/signin", authController.authUser);
-
-module.exports = router;
